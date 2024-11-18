a3845a8d3ae9d53e905b0cdf1a8b23fb614b5947
Chad Underhill <chad.j.underhill@gmail.com>
2024-11-18 18:37:35 -0500

    fix header rendering for standard size browsers

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Header: React.FC = () => {
   return (
     <header className="flex h-[110px] items-center justify-center text-white">
-      <div className="flex basis-1/2 justify-between h-full border-b-2 border-white border-opacity-20">
+      <div className="flex min-w-[1000px] basis-1/2 justify-between h-full border-b-2 border-white border-opacity-20">
         <div className="basis-1/2 flex items-center justify-center">
           <img src="/images/logos/logo_sanfrancisco49ers.svg" alt="49ers logo" className="size-[60px]" />
           <h1 className="pl-[20px] text-[45px] font-sans font-light">San Francisco 49ers</h1>
